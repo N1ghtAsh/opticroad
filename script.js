0c42f73178c32235bae46095735ff10e8c9c6432
Mehedi Hassan <mehedi.m04h@gmail.com>
2021-06-09 21:22:59 +0600

    Added All

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.navbar-nav>li>a').on('click', function(){
+   $('.navbar-collapse').collapse('hide');});
+var usrName=window.prompt("Enter Your Name");
+document.getElementById("newusr").innerHTML=usrName;
+
+//document.getElementById("new0").onclick=function (){alert(I will complete it later)};
