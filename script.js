597f3127ef4b3fec128e11897a0b29feca590204
Mehedi Hassan <mehedi.m04h@gmail.com>
2021-06-10 09:42:24 +0600

    new

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $('.navbar-nav>li>a').on('click', function(){
    $('.navbar-collapse').collapse('hide');});
-var usrName=window.prompt("Enter Your Name");
-document.getElementById("newusr").innerHTML=usrName;
+/*var usrName=window.prompt("Enter Your Name");
+document.getElementById("newusr").innerHTML=usrName;*/
 
 //document.getElementById("new0").onclick=function (){alert(I will complete it later)};
+var yorNme;
+function getNameValue(){
+yorNme=window.prompt("Enter your name:");
+document.getElementById("n1").innerHTML=yorNme;};
