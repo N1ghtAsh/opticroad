5c65cabb14ee508c87e93de7244b0ba65475d311
Mehedi Hassan <mehedi.m04h@gmail.com>
2021-06-18 14:33:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$('.navbar-nav>li>a').on('click', function(){
-   $('.navbar-collapse').collapse('hide');});
+$('.navbar-nav>li>a').on('click', function() {
+  $('.navbar-collapse').collapse('hide');
+});
 /*var usrName=window.prompt("Enter Your Name");
 document.getElementById("newusr").innerHTML=usrName;*/
 
@@ -7,8 +8,9 @@ document.getElementById("newusr").innerHTML=usrName;*/
 alert("Try it on Smarphones. It's not ready yet for big devices like Computer, laptop or tab...")
 //console.log("Try it on Smarphones. It's not ready yet for big devices like Computer, laptop or tab...")
 var yorNme;
-function getNameValue(){
-yorNme=window.prompt("Enter your name:");
-document.getElementById("n1").innerHTML=yorNme;
-//document.getElementByName("nme").input=yorNme;
+
+function getNameValue() {
+  yorNme = window.prompt("Enter your name:");
+  document.getElementById("n1").innerHTML = yorNme;
+  //document.getElementByName("nme").input=yorNme;
 };
